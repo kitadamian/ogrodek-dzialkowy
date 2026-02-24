e09ef536f92827971463873a96fce9a2d554cdbe
Damian Kita <kitadamian@wp.pl>
2026-02-24 11:02:17 +0100

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-# Inicjalizacja projektu Vite
-npm create vite@latest ogrodek-dzialkowy -- --template react
-cd ogrodek-dzialkowy
-npm install
-
-# Instalacja zależności dla gry
-npm install lucide-react
-npm install -D tailwindcss postcss autoprefixer
-npx tailwindcss init -p
